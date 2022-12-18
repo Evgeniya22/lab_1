c9e22ce44eac5b48acbfc323cb4d6c273e7189d0
Evgeniya22 <evgeniagolubeva2227@gmail.com>
2022-12-18 18:33:10 +0500

    lab4

--- laboratornie/lab4/example1.java
+++ laboratornie/lab4/example1.java
@@ -1,4 +1,17 @@
 package lab4;
 
 public class example1 {
+    public static void main(String[] args) {
+        int rows = 11;
+        int columns = 23;
+        int z;
+        for (int i = 1 ; i <= rows; i++){
+            z = 0;
+            for (int j = 1; j <=columns; j++){
+                System.out.print("+");
+                z = z + 1;
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ laboratornie/timus_1787/task_1787.java
@@ -0,0 +1,2 @@
+package timus_1787;
+
