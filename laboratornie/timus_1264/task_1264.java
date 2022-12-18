be3e8a82bb09e1ab534050ae8c1e7ac455cd2e15
Evgeniya22 <evgeniagolubeva2227@gmail.com>
2022-12-18 16:51:17 +0500

    timus_2001

---
+++ laboratornie/timus_1264/task_1264.java
@@ -0,0 +1,18 @@
+package timus_1264;
+
+import java.util.Scanner;
+
+public class task_1264 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int number0fM = m + 1;
+
+        int result = n * number0fM;
+
+        System.out.println (result);
+    }
+
+}
