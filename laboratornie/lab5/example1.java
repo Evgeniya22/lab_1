d881552305b706c9e589e56c5e0864942a1b9956
Evgeniya22 <evgeniagolubeva2227@gmail.com>
2022-12-25 14:24:08 +0500

    lab5

---
+++ laboratornie/lab5/example1.java
@@ -0,0 +1,29 @@
+package lab5;
+
+public class example1 {
+    public static void main(String[] args) {
+        Example Example1 = new Example();
+        Example1.viewCode();
+
+        Example1.Set('A');
+        System.out.println("Char code = " + Example1.getCodeSymbol()+"\n");
+
+        Example1.viewCode();
+    }
+}
+class Example {
+    private char ch;
+    public void Set(char ch1) {
+        this.ch = ch1;
+    }
+    public int getCodeSymbol() {
+        return (int) ch;
+    }
+    public void viewCode() {
+        System.out.println("ch1 = " + ch);
+        //int code = ch;
+        System.out.println("ch1 = " + getCodeSymbol()+"\n");
+    }
+}
+
+
