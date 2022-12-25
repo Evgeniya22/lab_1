1834ddb1076504114992a435f7cd1f37fc0a9c52
Evgeniya22 <evgeniagolubeva2227@gmail.com>
2022-12-25 14:00:58 +0500

    timus_2066

---
+++ laboratornie/timus_2066/task_2066.java
@@ -0,0 +1,19 @@
+package timus_2066;
+
+import java.util.Scanner;
+
+public class task_2066 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        if (b ==0 || b == 1 || c == 1) {
+            System.out.println( a - b -c);
+        } else {
+            System.out.println( a -b * c);
+        }
+    }
+}
